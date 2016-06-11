dcd985829e68d1c33b675d40093d2e0690346f27
Gergana Yordanova <gergana.yordanova@gmail.com>
2016-06-11 10:38:00 +0300

    Added method to generate obstacles in the map

--- GameShadow/GameShadow/GameLogic/GameInitializer.cs
+++ GameShadow/GameShadow/GameLogic/GameInitializer.cs
@@ -7,10 +7,34 @@ namespace GameShadow.GameLogic
     {
         public const int FieldWidth = 12;
         public const int FieldLength = 12;
+        public const int MovableTerrainOdds = 90;
         public const int NoSpawnRadius = 3;
         public const int MonsterCount = 5;
 
         private static readonly int MaxFieldPosition = FieldWidth * FieldLength - 1;
+
+        static void GenerateObstacles(Game game)
+        {
+            
+            Random rnd = new Random();
+
+            for (int y = 0; y < FieldWidth; y++)
+            {
+                for (int x = 0; x < FieldLength; x++)
+
+                {
+                    int r = rnd.Next(0, 100);
+                    game.Field[x, y] = (r < MovableTerrainOdds) ? 0 : 1;
+                    if (game.Field[x, y] == 1)
+                    {
+                        game.ObstaclesByPosition.Add((y * FieldLength + x),true);
+                    }
+                    else game.ObstaclesByPosition.Add((y * FieldLength + x), false);
+                }
+            }
+
+        }
+
 
         public static void SpawnMonsters(Game game)
         {
@@ -19,16 +43,18 @@ namespace GameShadow.GameLogic
             {
                 int position = rnd.Next(0, MaxFieldPosition);
                 int positionX = position % FieldLength;
-                int positionY = position / FieldWidth;
-
-                int distanceToPlayer =
-                    Math.Abs(positionX * positionX + positionY * positionY -
-                    game.Player.PositionX * game.Player.PositionX + positionY * positionY);
-
-                if(distanceToPlayer > NoSpawnRadius * NoSpawnRadius)
+                int positionY = position / FieldLength;
+                if (game.Field[positionX, positionY] == 0)
                 {
-                    Monster monster = new Monster(positionX, positionY);
-                    game.Monsters.Add(monster);
+                    int distanceToPlayer =
+                        Math.Abs(positionX * positionX + positionY * positionY -
+                        game.Player.PositionX * game.Player.PositionX + positionY * positionY);
+
+                    if (distanceToPlayer > NoSpawnRadius * NoSpawnRadius)
+                    {
+                        Monster monster = new Monster(positionX, positionY);
+                        game.Monsters.Add(monster);
+                    }
                 }
             }
         }
