6017ebf35f6f2045ae5b126c0837dfa44ceccbb5
Gergana Yordanova <gergana.yordanova@gmail.com>
2016-06-12 12:13:59 +0300

    added empty game constructor

--- GameShadow/GameShadow/GameData/Game.cs
+++ GameShadow/GameShadow/GameData/Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -34,6 +34,9 @@ namespace GameShadow.GameData
             Player = player;
             Monsters = monsters;
         }
-
+	  public Game()
+		{
+		}
     }
+
 }
