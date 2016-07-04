2ce8199b4fe762682e133fb10a9ef58bcf9c27c1
Gergana Yordanova <gergana.yordanova@gmail.com>
2016-07-04 21:15:09 +0300

    Added shooting interface

---
+++ EmojiHunter/EmojiHunter/GameData/IShooting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmojiHunter.GameData
+{
+    interface IShooting
+    {
+        int Damage { get; set; }
+
+        float ShootingSpeed { get; }
+
+        float ShootingDelay { get;  }
+
+        float SightSpeed { get;  }
+
+    }
+}
