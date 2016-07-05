bc66b042a4a5d0233f3a9696ed75f3f955c2d8b7
Gergana Yordanova <gergana.yordanova@gmail.com>
2016-07-05 19:06:50 +0300

    Add some emoticon classes

--- EmojiHunter/EmojiHunter/GameData/Emoticons/GrinEmoticon.cs
+++ EmojiHunter/EmojiHunter/GameData/Emoticons/GrinEmoticon.cs
@@ -2,8 +2,23 @@
 {
     public class GrinEmoticon : GoodEmoticon
     {
-        public GrinEmoticon(string name) : base(name)
-        {
-        }
+            private const int DamageBoostValue = 5;
+
+            private const int DefaultHealth = 100;
+
+            private const int DefaultArmor = 20;
+
+            private const float DefaultMovementSpeed = 1;
+
+            private readonly Reward DefaultReward = new Reward(0, 0, DamageBoostValue, 0, 0);
+
+            public GrinEmoticon(string name) : base(name)
+            {
+                this.Health = DefaultHealth;
+                this.Armor = DefaultArmor;
+                this.MovementSpeed = MovementSpeed;
+                this.Reward = DefaultReward;
+            }
+      
     }
 }
