993f7d01fa5dd4f1729f023aa9763fee3e046e47
svetljov <svetljo@gmail.com>
2016-08-03 11:38:29 +0300

    Crazy move added

--- EmojiHunter/EmojiHunter/GUIModels/UIEmoticonMoveBehaviors/CrazyMoveBehavior.cs
+++ EmojiHunter/EmojiHunter/GUIModels/UIEmoticonMoveBehaviors/CrazyMoveBehavior.cs
@@ -11,13 +11,33 @@
 
         public override void Move(UIEmoticon uiEmoticon)
         {
-            // TO DO
-            // You have access to this.UIHero.Position
-            // You have access to uiEmoticon
-            // uiEmoticon.Sprite.Position;
-            // uiEmoticon.Direction;
-            // uiEmoticon.Position;
-            // uiEmoticon.GameObject.State.MovementSpeed;
+            Random randomNumber = new Random();
+            int directionXComponent = randomNumber.Next(-10, 10);
+            int directionYComponent = randomNumber.Next(-10, 10);
+            int movementSpeed = randomNumber.Next(-10, 10);
+
+            var direction = new Vector2(directionXComponent, directionYComponent);
+            direction.Normalize();
+            uiEmoticon.Direction = direction;
+            uiEmoticon.Position += uiEmoticon.Direction * movementSpeed;
+            uiEmoticon.Sprite.Position = uiEmoticon.Position;
+
+            if (uiEmoticon.Position.X < 0)
+            {
+                uiEmoticon.Position += new Vector2(10, 0);
+            }
+            else if (uiEmoticon.Position.X > FieldWidth)
+            {
+                uiEmoticon.Position -= new Vector2(10, 0);
+            }
+            else if (uiEmoticon.Position.Y < 0)
+            {
+                uiEmoticon.Position += new Vector2(0, 10);
+            }
+            else if (uiEmoticon.Position.Y > FieldHeight)
+            {
+                uiEmoticon.Position -= new Vector2(0, 10);
+            }
         }
     }
 }
